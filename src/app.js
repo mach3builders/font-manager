a12e612c691b8d6e598ea3986fe912924c06d110
Paul Kruijt <pkruijt@mach3builders.nl>
2021-05-03 17:01:07 +0200

    wip

--- src/app.js
+++ src/app.js
@@ -12,37 +12,8 @@ document.addEventListener('DOMContentLoaded', function()
 	const $fontManager	= document.querySelector('font-manager');
 
 	if ($handlers.length && $fontManager) {
-
-		/*
-		 * Initialize the Vue Instance
-		 */
-		const fontData = {
-			"kind": "webfonts#webfontList",
-			"items": [
-				{
-					"kind": "webfonts#webfont",
-					"family": "ABeeZee",
-					"category": "sans-serif",
-					"variants": [
-						"regular",
-						"italic"
-					],
-					"subsets": [
-						"latin"
-					],
-					"version": "v9",
-					"lastModified": "2016-09-29",
-					"files": {
-						"regular": "http://fonts.gstatic.com/s/abeezee/v9/mE5BOuZKGln_Ex0uYKpIaw.ttf",
-						"italic": "http://fonts.gstatic.com/s/abeezee/v9/kpplLynmYgP0YtlJA3atRw.ttf"
-					}
-				}
-			]
-		};
-
 		var vm = new Vue({
 			el: '#app',
-			//data: fontData,
 			components: {
 				FontManager
 			}
